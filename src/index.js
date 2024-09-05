d4b299c454c6d69a8fd050e6280154d81606b372
Tatiana Vasylieva <tatiana@Tatianas-Laptop.local>
2024-09-05 19:06:07 +0200

    Add import

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import './styles/home.scss';
 import './styles/marquee.scss';
+import './styles/_media.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
